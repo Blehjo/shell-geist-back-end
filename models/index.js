88df03483115f18d3a950cc0667e6a18887661c3
bseton11-YJKnqQ <bseton11@gmail.com>
2022-11-19 12:06:37 -0600

    added folder structure and initialized git repository

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const UserProfile = require('./UserProfile');
+const UserPost = require('./UserPost');
+const Friendship = require('./Friendship');
+const PostComment = require('./PostComment');
+const PostLike = require('./PostLike');
+
+module.exports = { UserProfile, UserPost, Friendship, PostComment, PostLike };
