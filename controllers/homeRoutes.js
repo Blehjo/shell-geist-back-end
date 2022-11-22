bdafd60d872bb8dfa2d478f242c4fd293aa79bc6
bseton11-YJKnqQ <bseton11@gmail.com>
2022-11-21 21:10:02 -0600

    added userprofile seeds

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const { UserProfile, UserPost, Friendship, PostLike, PostComment } = require('../models');
+const isAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await UserPost.findAll({
+            include: [
+                {
+                    model: UserProfile,
+                    attributes: ['username'],
+                }
+            ],
+            order: [
+                ['created_date_time', 'DESC'],
+            ],
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }))
+        // console.log(posts)
+        res.render('homepage', {
+            posts
+        })
+        console.log(posts)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+router.get('/menu', (req, res) => {
+    res.render('menu', {
+        logged_in: req.session.user_id
+
+    })
+})
+
+module.exports = router;
