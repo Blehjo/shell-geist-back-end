86218901a6eedf504fe16370f71c619a25bda2e1
Blehjo <blehjooo@gmail.com>
2022-12-01 00:31:21 -0600

    updated conversation routes to include users. this way a user will only see the conversations that he or she is in.

--- controllers/api/conversationRoutes.js
+++ controllers/api/conversationRoutes.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
-const { Conversation } = require('../../models');
+const { Conversation, Message } = require('../../models');
 
 router.get('/', async (req, res) => {
     try {
         const commentData = await Conversation.findAll({
+            attributes: ['id'],
+            include: {
+                model: Message,
+                attributes: ['from_profile', 'message_text', 'sent_datetime']
+            },
             order: [
                 ['id', 'DESC'],
             ],
@@ -16,6 +21,29 @@ router.get('/', async (req, res) => {
         console.log(posts)
     } catch (err) {
         res.status(500).json(err)
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/login');
+    } else {
+        try {
+            const conversationdata = await Conversation.findByPk(req.params.id, {
+                attributes: ['id'],
+            include: {
+                model: Message,
+                attributes: ['from_profile', 'message_text', 'sent_datetime']
+            },
+            order: [
+                ['id', 'DESC'],
+            ],
+            });
+            res.json(conversationdata);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
     }
 });
 
