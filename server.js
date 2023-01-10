9341295112ade3c69af5beb77156c095ad3bf440
bseton11-YJKnqQ <bseton11@gmail.com>
2023-01-10 11:10:06 -0600

    adding headers

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(session(sess));
 
 app.use(cors());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin");
+  res.header("Access-Control-Allow-Origin", "https://shellgeistapi.herokuapp.com/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
