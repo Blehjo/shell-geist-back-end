bd6af61f2265ca5915d4a7753d324a0ac005e9da
bseton11-YJKnqQ <bseton11@gmail.com>
2023-01-20 12:53:56 -0600

    enabled cors

--- server.js
+++ server.js
@@ -29,19 +29,7 @@ let corsOptions = {
 };
 
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://shellgeist.com", "https://shellgeistapi.herokuapp.com");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-  res.header("Access-Control-Allow-Credentials", true); 
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-  next();
-});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
