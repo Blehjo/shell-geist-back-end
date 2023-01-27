3801eca74288447b0db230174c7008a5344c06cb
bseton11-YJKnqQ <bseton11@gmail.com>
2023-01-27 14:49:23 -0600

    fixed login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -76,7 +76,7 @@ router.post('/', async (req, res) => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
 
-      res.status(200).json(userData);
+      res.status(200).json({ user: userData, message: 'You are now signed up!' });
     });
   } catch (err) {
     res.status(400).json(err);
