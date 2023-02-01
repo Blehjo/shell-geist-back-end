bf505ab5ae5ff4a12e5499aec569ac2f131b01a0
Blehjo <blehjooo@gmail.com>
2023-02-01 11:46:45 -0600

    added cookies

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: { secure: true },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -28,12 +28,13 @@ app.use(cors({
     "https://shellgeist.com", "http://localhost:3000"
   ],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true
 }));
 
 app.use(session(sess));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
